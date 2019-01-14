9f47850c3e7089c88c4d3e66dad8d3198661f47d
Lili <liliszendrei@gmail.com>
2019-01-14 14:12:04 +0100

    d2

--- src/Future.java
+++ src/Future.java
@@ -1,6 +1,6 @@
 public class Future {
 
-    public static void future() {
-        System.out.println("Aquarius");
+    public static void future(String horoscope) {
+        System.out.println(horoscope);
     }
 }
