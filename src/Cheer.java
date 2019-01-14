c76682c619fca9902dc9023daf9a28a0ab22def4
Lili <liliszendrei@gmail.com>
2019-01-14 14:55:07 +0100

    d6

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
-  public static void cheer(){
-    System.out.println("Java!!!");
+  public static void cheer(String s){
+    System.out.println("Cheer for the " + s);
   }
 }
